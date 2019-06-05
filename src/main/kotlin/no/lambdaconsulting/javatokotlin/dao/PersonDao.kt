ba6c31d74c1c3f7fb184a43be4d9d57a042b1fdc
geirem <geiremb@gmail.com>
2019-06-05 11:39:23 +0000

    Add files via upload

---
+++ src/main/kotlin/no/lambdaconsulting/javatokotlin/dao/PersonDao.kt
@@ -0,0 +1,8 @@
+package no.lambdaconsulting.javatokotlin.dao
+import no.lambdaconsulting.javatokotlin.domain.PersonSummary
+
+
+interface PersonDao {
+    fun findAll(): List<PersonSummary>
+    fun getById(id: Int): PersonSummary
+}
