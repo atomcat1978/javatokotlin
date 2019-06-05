ad861d0787b2c556b165b2496a167892d66673e2
Flogstad <peter.flogstad@gmail.com>
2019-06-05 13:09:17 +0200

    Replaced Java PetSummary, PetService with Kotlin equivalents

---
+++ src/main/kotlin/no/lambdaconsulting/javatokotlin/service/PetService.kt
@@ -0,0 +1,19 @@
+package no.lambdaconsulting.javatokotlin.service
+
+import no.lambdaconsulting.javatokotlin.dao.BreedDao
+import no.lambdaconsulting.javatokotlin.dao.PetDao
+import no.lambdaconsulting.javatokotlin.domain.PetDetails
+import org.springframework.stereotype.Service
+
+@Service
+class PetService(private val petDao: PetDao, private val breedDao: BreedDao) {
+
+    fun getDetails(id: Int) = petDao.getById(id).let { petSummary ->
+        PetDetails(
+                petSummary.id,
+                petSummary.name,
+                breedDao.getById(petSummary.breedId)
+        )
+    }
+
+}
